5471dd2ffbe8459e2ea1121fc329e79891cfd608
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-15 18:25:04 -0500

    Update 11.0_Jedi_Training.py

--- 11.0_Jedi_Training.py
+++ 11.0_Jedi_Training.py
@@ -1,5 +1,5 @@
 '''
-# 11.0 Jedi Training (45pts)  Name:________________
+# 11.0 Jedi Training (50pts)  Name:________________
  
 CHAPTER 11 FINAL CODE QUESTIONS: (10pts)
 --------------------------------
@@ -22,4 +22,34 @@ CHAPTER 11 FINAL CODE QUESTIONS: (10pts)
 9.) What is this code checking?  self.pos_y > SH - self.rad:
 
 10.) What is this code checking? if self.pos_x < self.rad
+'''
+
+
+
+
+
+
+'''
+30 BOX BOUNCE PROGRAM (20pts)
+---------------------
+You will want to incorporate lists to modify the
+Ball Bounce Program to create the following:
+
+1.) Screen size 600 x 600
+2.) Draw four 30px wide side rails on all four sides of the window
+3.) Make each side rail a different color.
+4.) Draw 30 black boxes(squares) of random size from 10-50 pixels
+5.) Animate them starting at random speeds from -300 to +300 pixels/second. 
+6.) All boxes must be moving.
+7.) Start all boxes in random positions between the rails.
+8.) Bounce boxes off of the side rails when the box edge hits the side rail.
+9.) When the box bounces change its color to the rail it just hit.
+10.)Title the window 30 Boxes
+
+Helpful Hints:
+1.) When you initialize the MyGame class create an empty list called self.boxlist=[] to hold all of your boxes.
+2.) Then use a for i in range(30): list to instantiate boxes and append them to the list.
+3.) In the on_draw section use: for box in self.boxlist: box.draw_box()
+4.) Also in the on_draw section draw the side rails.
+5.) In the on_update section use: for box in self.boxlist: box.update_box()
 '''
